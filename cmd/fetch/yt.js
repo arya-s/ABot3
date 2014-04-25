14c0f10462a6b460fb677d02c63cd760e8e003ab
arya-s <andrei.borza@gmail.com>
2014-04-26 05:25:05 +0900

    Nope.

--- cmd/fetch/yt.js
+++ cmd/fetch/yt.js
@@ -5,16 +5,19 @@ var youtube = require('youtube-node');
 youtube.setKey(config.youtube.key);
 
 module.exports = function(irc){
-    youtube.getById(getYoutubeID(irc.message), function(result){
-        if(result){
-            var videoTitle = result.items[0].snippet.title;
-            var durationString = util.convertTime(result.items[0].contentDetails.duration);
-            var channelTitle = result.items[0].snippet.channelTitle;
-            var likes = result.items[0].statistics.likeCount;
-            var dislikes = result.items[0].statistics.dislikeCount;
-            var viewCount = result.items[0].statistics.viewCount;
+    var id = getYoutubeId(irc.message);
+    if(id){
+        youtube.getById(id, function(result){
+            if(result){
+                var videoTitle = result.items[0].snippet.title;
+                var durationString = util.convertTime(result.items[0].contentDetails.duration);
+                var channelTitle = result.items[0].snippet.channelTitle;
+                var likes = result.items[0].statistics.likeCount;
+                var dislikes = result.items[0].statistics.dislikeCount;
+                var viewCount = result.items[0].statistics.viewCount;
 
-            irc.client.say(irc.to, videoTitle + ' (' + durationString + ') by ' + channelTitle + ' [+' + likes + '/-' + dislikes + ', ' + viewCount + ' views]'); 
-        }
-    });
+                irc.client.say(irc.to, videoTitle + ' (' + durationString + ') by ' + channelTitle + ' [+' + likes + '/-' + dislikes + ', ' + viewCount + ' views]'); 
+            }
+        });
+    }
 };
