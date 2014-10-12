383dc4d764cc4fd11d8cef7634d95ec275875636
arya-s <andrei.borza@gmail.com>
2014-10-13 02:28:11 +0900

    Stupid mistake.

--- cmd/execute/addquote.js
+++ cmd/execute/addquote.js
@@ -2,13 +2,12 @@ var util = require('../../lib/util.js');
 
 module.exports = function(irc){
     var quote = util.trim(irc.message);
-    console.log(quote.length);
 
     if(quote.length > 0){
         irc.db.addNewQuote(quote, function(err, quoteId){
            if(!err){
                console.log(quoteId);
-               irc.client.say(irc.to, 'Added your shitty quote as quote #', quoteId);
+               irc.client.say(irc.to, 'Added your shitty quote as quote #' + quoteId);
            } else {
                irc.client.say(irc.to, 'Something went wrong, try again later, or don\'t, I don\'t care.');
            }
